7cfadfa05314b668cbd157635bc327db70afc199
Andrew <andrew.panix@gmail.com>
2020-07-30 09:53:53 +0300

    Update _item.php

--- views/default/_item.php
+++ views/default/_item.php
@@ -1,28 +1,29 @@
 <?php
 use panix\engine\Html;
+use panix\engine\CMS;
 
+/**
+ * @var \panix\mod\news\models\News $model
+ * @var \yii\web\View $this
+ */
 
 ?>
 
-<div class="single_blog">
-    <div class="blog_thumb">
-        <a href="blog-details.html"><img src="assets/img/blog/blog6.jpg" alt=""></a>
+<div class="card mb-3">
+    <div class="card-header">
+        <?= Html::a($model->name, $model->getUrl(), ['class' => 'h3']); ?>
+        <span class="float-right">
+            <i class="icon-time"></i>
+            <small><?= CMS::date($model->created_at); ?></small>
+        </span>
     </div>
-    <div class="blog_content">
-        <div><?= Html::a($model->name,$model->getUrl(),['class'=>'h3']);?></div>
-        <div class="blog_meta">
-            <span class="post_date"><i class="fa-calendar fa"></i> Februaey 02, 2019</span>
-            <span class="author"><i class="fa fa-user-circle"></i> Posts by : admin</span>
-            <span class="category">
-                                        <i class="fa fa-folder-open"></i>
-                                        <a href="#">Fashion</a>
-                                    </span>
-        </div>
-        <div class="blog_desc">
+    <div class="card-body">
+        <div>
             <?= $model->isText('short_description'); ?>
         </div>
-        <div class="readmore_button d-none">
-            <a href="blog-details.html">read more</a>
-        </div>
+    </div>
+    <div class="card-footer">
+        <span class="views mr-3"><i class="icon-eye"></i> <span><?= $model->views; ?></span></span>
+        <span class="author mr-3"><i class="icon-user-outline"></i> <?= $model->user->username; ?></span>
     </div>
 </div>
